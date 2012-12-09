1d37f4fb2c7c4fbae6a6c159baaf7791e20f6ebf
pi <ncvc@mit.edu>
2012-12-09 19:51:49 +0000

    Cleaned up the code

--- Animator.py
+++ Animator.py
@@ -4,6 +4,7 @@ import Strip
 import Queue
 import threading
 import os.path
+import socket
 
 socket_name = "/tmp/thechillsocket"
 
@@ -12,9 +13,8 @@ class Animator(SocketServer.UnixStreamServer, object):
         super(Animator, self).__init__(socket_name, handler)
         self.queue = Queue.Queue()
         strip = Strip.Strip(32)
-#        strip.enableXmasMode()
         self.stepper = Stepper(self.queue, Animation.Animation(strip))
-#        self.stepper.start()
+        self.stepper.start()
         self.animations = {'colorchase' : Animation.ColorChase(strip),
                            'colorwipe' : Animation.ColorWipe(strip),
                            'blackout' : Animation.Blackout(strip),
@@ -31,15 +31,9 @@ class Animator(SocketServer.UnixStreamServer, object):
 
 class AnimationRequestHandler(SocketServer.StreamRequestHandler):
     def handle(self):
-        # self.rfile is a file-like object created by the handler;
-        # we can now use e.g. readline() instead of raw recv() calls
         self.data = self.rfile.readline().strip()
-#        print "{} wrote:".format(self.client_address[0])
         print "Received:     {}".format(self.data)
-        #self.server.addToQueue(self.data)
-        # Likewise, self.wfile is a file-like object used to write back
-        # to the client
-#        self.wfile.write(self.data.upper())
+        self.server.addToQueue(self.data)
 
 class Stepper(threading.Thread, object):
     def __init__(self, queue, anim):
@@ -64,51 +58,32 @@ class Stepper(threading.Thread, object):
         self.stop_request.set()
         super(Stepper, self).join(timeout)
 
-
-import socket
-import sys
-import os.path
-
 def sendMessage(message):
-    if os.path.exists(socket_name):
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-        try:
-            # Connect to server and send data
-            sock.connect(socket_name)
-            sock.send(message)
-            print "Sent:     {}".format(message)
-
-            # Receive data from the server and shut down
-            # received = sock.recv(1024) 
-        except IOError as e:
-            if e.errno == 13:
-                print "{0}\nTry again using sudo".format(e)
-            elif e.errno == 111:
-                print "{0}\nError connecting to socket {1}\nMake sure the server is running and is on this socket".format(e, socket_name)
-            else:
-                print e
-        except Exception as e:
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    try:
+        sock.connect(socket_name)
+        sock.send(message)
+        print "Sent:     {}".format(message)
+    except IOError as e:
+        if e.errno == 13:
+            print "{0}\nTry again using sudo".format(e)
+        elif e.errno == 111:
+            print "{0}\nError connecting to socket {1}\nMake sure the server is running and is on this socket".format(e, socket_name)
+        else:
             print e
-        finally:
-            sock.close()
-    else:
-        print "Couldn't connect to socket"
+    except Exception as e:
+        print e
+    finally:
+        sock.close()
 
 if __name__ == "__main__":
     print "Starting server"
     try:
         if os.path.exists(socket_name):
             os.remove(socket_name)
-
-            # Create the server, binding to localhost on port 9999
             server = Animator(socket_name, AnimationRequestHandler)
-
-            # Activate the server; this will keep running until you
-            # interrupt the program with Ctrl-C
     except Exception as e:
-        print "Error starting server:\n" + str(e)
+        print "{0}\nError starting server:\n".format(e)
         raise
 
     print "Animation server is running on socket {0}".format(socket_name)
--- SocketTest.py
+++ SocketTest.py
@@ -1,36 +1,8 @@
+#tests the socket connection to the Animator
+#run as sudo, all arguments get passed as the message
+
 import Animator
 import sys
 
 if __name__ == "__main__":
     Animator.sendMessage(" ".join(sys.argv[1:]))
-
-"""
-
-import socket
-import sys
-import os.path
-
-def sendMessage(message):
-    socket_name = "/tmp/thechillsocket"
-    if os.path.exists(socket_name):
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-        try:
-            # Connect to server and send data
-            sock.connect(socket_name)
-            sock.send(message)
-            print "Sent:     {}".format(data)
-        
-            # Receive data from the server and shut down
-            #received = sock.recv(1024)
-        except Exception as e:
-            print "Error connecting to socket {0}.\nMake sure the server is running and is on this socket".format(socket_name)
-        finally:
-            sock.close()
-    else:
-        print "Couln't connect to socket."
-
-if __name__ == "__main__":    
-    sendMessage(" ".join(sys.argv[1:]))
-"""
